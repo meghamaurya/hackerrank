02e08d8c88ee2822b8ab40505a58985ff6fed327
megha <meghamini40@gmail.com>
2022-07-02 00:02:20 +0530

    Class vs. Instance

---
+++ day-4.js
@@ -0,0 +1,9 @@
+// Do some computations in here and print out the correct statement to the console
+
+if (age < 13) {
+    console.log("You are young.");
+} else if (age >= 13 && age < 18) {
+    console.log("You are a teenager.");
+} else {
+    console.log("You are old.");
+}
