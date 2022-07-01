a65e2d89d6eb5f6510e110142dc0b63bbccc1e8e
megha <meghamini40@gmail.com>
2022-07-02 00:22:47 +0530

    Loops

---
+++ day-5.js
@@ -0,0 +1,7 @@
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    for (let i = 1; i <= 10; i++) {
+        let result = n * i;
+        console.log(`${n} x ${i} = ${result}`);
+    }
+}
